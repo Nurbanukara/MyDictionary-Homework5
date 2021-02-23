d8e2a40c2ec9617d9a8b464d39c7c0f066bf3969
Nurbanu Kara <nurbanukr98@gmail.com>
2021-02-23 19:11:48 +0300

    Add project files.

---
+++ Day4-Homework5/MyDictionary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day4_Homework5
+{
+    class MyDictionary<TKey, TValue>
+    {
+        TKey[] keys;
+        TValue[] values;
+        public MyDictionary()
+        {
+            keys = new TKey[0];
+            values = new TValue[0];
+        }
+        public void Add(TKey key, TValue value)
+        {
+            TKey[] tempKey = keys;
+            TValue[] tempVal = values;
+            keys = new TKey[keys.Length + 1];
+            values = new TValue[values.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+            }
+            for (int i = 0; i < tempVal.Length; i++)
+            {
+                values[i] = tempVal[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public void ToList()
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine(keys[i] + " , " + values[i]);
+            }
+        }
+    }
+}
---
+++ Day4-Homework5/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Day4_Homework5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            System.Console.WriteLine("--------------MyDictionary-------------");
+            myDictionary.Add(1, "Nurbanu Kara");
+            myDictionary.Add(2, "Belgin Kaya");
+            myDictionary.Add(3, "Derya Malçok");
+            myDictionary.ToList();
+        }
+    }
+}
